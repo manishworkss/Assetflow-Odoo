84adb080bb4d1685a18e33fba25fdfa083273c97
YUGRAJ MEWARA <yugrajmewara08@gmail.com>
2026-07-12 15:29:16 +0530

    chore: remove unused Email import from GoogleAuthDto

--- src/main/java/com/assetflow/backend/dto/GoogleAuthDto.java
+++ src/main/java/com/assetflow/backend/dto/GoogleAuthDto.java
@@ -1,6 +1,5 @@
 package com.assetflow.backend.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
