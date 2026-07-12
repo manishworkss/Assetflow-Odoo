97cfea7ee14f991fed040f0c34883ff40285d810
YUGRAJ MEWARA <yugrajmewara08@gmail.com>
2026-07-12 10:44:25 +0530

    Initial backend setup

---
+++ src/main/java/com/assetflow/backend/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.assetflow.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI assetFlowOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("AssetFlow API")
+                        .description("API Documentation for AssetFlow Enterprise Asset & Resource Management System")
+                        .version("v1.0.0")
+                        .contact(new Contact().name("Hackathon Team"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
---
+++ src/main/java/com/assetflow/backend/constants/AppConstants.java
@@ -0,0 +1,17 @@
+package com.assetflow.backend.constants;
+
+public class AppConstants {
+    
+    // Status Messages
+    public static final String SUCCESS = "Operation completed successfully";
+    public static final String ERROR = "An error occurred";
+    
+    // Default values
+    public static final String ASSET_TAG_PREFIX = "AF-";
+
+    // You can add more constants here...
+    
+    private AppConstants() {
+        // Prevent instantiation
+    }
+}
